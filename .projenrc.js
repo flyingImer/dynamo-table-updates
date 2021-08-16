5433d67eba941bc847a3c699bdb0405596389944
flyingImer <ej.wang.devs@gmail.com>
2021-08-16 23:07:04 +0000

    chore: front a DynamoDB table with API Gateway Rest Api for CRUD

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,36 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, ProjectType } = require('projen');
+
+const DEP_CORE_PKG_NAME = '@aws-solutions-constructs/core';
+const DEPS_COMPATIBLE_CDK_VERSION = process.env.CDK_DEP_VERSION || '1.117.0';
+
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: DEPS_COMPATIBLE_CDK_VERSION,
   defaultReleaseBranch: 'main',
   name: 'dynamo-table-updates',
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // get the latest dep version (essentially the latest CDK version it supports)
+  projenCommand: `CDK_DEP_VERSION=$(yarn info ${DEP_CORE_PKG_NAME} version -s) npx projen`,
+
+  cdkDependencies: [
+    '@aws-cdk/aws-dynamodb',
+  ], /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  deps: [
+    `${DEP_CORE_PKG_NAME}@${DEPS_COMPATIBLE_CDK_VERSION}`,
+    `@aws-solutions-constructs/aws-apigateway-dynamodb@${DEPS_COMPATIBLE_CDK_VERSION}`,
+    `@aws-solutions-constructs/aws-dynamodbstreams-lambda@${DEPS_COMPATIBLE_CDK_VERSION}`,
+  ], /* Runtime dependencies of this module. */
+  description: 'An AWS CDK application (fronted by API GW) which integrates the DynamoDB table with stream solutions for near real-time updates to downstream.', /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  projectType: ProjectType.APP, /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
 });
+
+// FIXME: to work with aws-solutions-constructs today, which only sits on CDK version 1.117.0, forcing this project CDK version to the same
+// blocker issue: https://github.com/awslabs/aws-solutions-constructs/issues/117
+project.deps.all.filter(dep => dep.name.includes('aws-cdk')).forEach(dep => {
+  project.deps.removeDependency(dep.name);
+  project.deps.addDependency(`${dep.name}@${DEPS_COMPATIBLE_CDK_VERSION}`, dep.type);
+});
+
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
-import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import { Table } from '@aws-cdk/aws-dynamodb';
+import { App, Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { ApiGatewayToDynamoDB } from '@aws-solutions-constructs/aws-apigateway-dynamodb';
+import { DefaultTableProps } from '@aws-solutions-constructs/core';
 
-export class MyStack extends Stack {
+export class DynamoUpdatesStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
     // define resources here...
+    const table = new Table(this, 'Table', {
+      ...DefaultTableProps,
+      // TODO: enable stream
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    new ApiGatewayToDynamoDB(this, 'Gateway', {
+      existingTableObj: table,
+      logGroupProps: {
+        removalPolicy: RemovalPolicy.DESTROY,
+      },
+      allowReadOperation: true,
+      allowCreateOperation: true,
+      allowDeleteOperation: true,
+      allowUpdateOperation: true,
+      createRequestTemplate: "{\r\n  \"TableName\": \"${Table}\",\r\n  \"Item\": {\r\n    \"id\": {\r\n      \"S\": \"$input.path('$.id')\"\r\n    },\r\n    \"EventCount\": {\r\n      \"N\": \"$input.path('$.EventCount')\"\r\n    },\r\n    \"Message\": {\r\n      \"S\": \"$input.path('$.Message')\"\r\n    }\r\n  }\r\n}",
+      updateRequestTemplate: "{\r\n  \"TableName\": \"${Table}\",\r\n  \"Key\": {\r\n    \"id\": {\r\n      \"S\": \"$input.path('$.id')\"\r\n    }\r\n  },\r\n  \"ExpressionAttributeValues\": {\r\n    \":event_count\": {\r\n      \"N\": \"$input.path('$.EventCount')\"\r\n    },\r\n    \":message\": {\r\n      \"S\": \"$input.path('$.Message')\"\r\n    }\r\n  },\r\n  \"UpdateExpression\": \"ADD EventCount :event_count SET Message = :message\",\r\n  \"ReturnValues\": \"ALL_NEW\"\r\n}",
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+new DynamoUpdatesStack(app, 'DynamoUpdatesStack');
 
 app.synth();
